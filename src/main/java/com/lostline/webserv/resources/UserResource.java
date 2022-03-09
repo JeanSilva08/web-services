afb697caa7d86ebe81aba3b8abadf519c348141a
Jean$ <jeancarlossoares.s@gmail.com>
2022-03-09 17:47:06 -0300

    User entity and resource

---
+++ src/main/java/com/lostline/webserv/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.lostline.webserv.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lostline.webserv.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail,con", "9999999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
