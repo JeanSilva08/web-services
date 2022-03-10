6014cfe089a2d3c40111c135a89d980f2ad6131c
Jean$ <jeancarlossoares.s@gmail.com>
2022-03-10 16:43:14 -0300

    Service Layer, component registration

---
+++ src/main/java/com/lostline/webserv/services/UserService.java
@@ -0,0 +1,26 @@
+package com.lostline.webserv.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lostline.webserv.entities.User;
+import com.lostline.webserv.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
